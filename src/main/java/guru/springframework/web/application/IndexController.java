adb489f1df26523efb207e8ad8b0efdc5420fbdc
mohammedsaad <mohammed912009@hotmail.com>
2019-12-13 11:36:35 +0100

    Revert "refactoring"
    This reverts commit 53985441e162a610918c26a76332418e7e185b10.

--- src/main/java/guru/springframework/web/application/IndexController.java
+++ src/main/java/guru/springframework/web/application/IndexController.java
@@ -5,7 +5,7 @@ import guru.springframework.business.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by jt on 6/1/17.
@@ -20,12 +20,12 @@ public class IndexController {
         this.recipeService = recipeService;
     }
 
-    @GetMapping({"", "/", "/index"})
+    @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
         log.debug("Getting Index page");
 
         model.addAttribute("recipes", recipeService.getRecipes());
-        System.out.println("successful");
+
         return "index";
     }
 }
